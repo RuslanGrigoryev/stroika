1c07b94dae3345b7e79f3a1010f754782386794b
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-09-07 16:13:00 +0300

    Добавил index

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
 $(function () {
-	/*main menu*/
-
-	var $link = $('.menu__item').find('a');
-
-	$link.on('click', function (e) {
-		e.preventDefault();
-		$('.menu__item').removeClass('menu__item_active');
-		$(this).closest('.menu__item').toggleClass('menu__item_active');
-	});
-
 	/*advanced menu*/
 
 	var $linkAdv = $('.menu__advanced__item').find('a');
